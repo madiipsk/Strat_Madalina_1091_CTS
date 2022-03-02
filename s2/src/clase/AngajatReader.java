a54cb8aaf6c1ece79ec832f58adc9461a809fb95
madiipsk <madalina.gaby8@gmail.com>
2022-03-02 19:20:37 +0200

    add claseReader sem2

---
+++ s2/src/clase/AngajatReader.java
@@ -0,0 +1,30 @@
+package clase;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AngajatReader extends AplicantReader{
+
+    @Override
+    public List<Aplicant> readAplicants(String file) throws FileNotFoundException {
+        Scanner input2 = new Scanner(new File(file));
+        input2.useDelimiter(",");
+        List<Aplicant> angajati = new ArrayList<Aplicant>();
+
+        while (input2.hasNext()) {
+            Angajat angajat=new Angajat();
+            readAplicant(input2,angajat);
+            int salariu = input2.nextInt();
+            String ocupatie = input2.next();
+            angajat.setSalariu(salariu);
+            angajat.setOcupatie(ocupatie);
+            angajati.add(angajat);
+        }
+        input2.close();
+        return angajati;
+    }
+
+}
