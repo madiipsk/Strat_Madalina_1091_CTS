d55dbd48e00437ec289a30468c883c30c000eab9
madiipsk <madalina.gaby8@gmail.com>
2022-04-06 17:39:48 +0300

    add facade s6

---
+++ facade/src/clase/Autobuz.java
@@ -0,0 +1,22 @@
+package clase;
+
+public class Autobuz {
+    public void deschideUsaFata(){
+        System.out.println("Usa fata deschisa!");
+    }
+    public void deschideUsaMijloc(){
+        System.out.println("Usa mijloc deschisa!");
+    }
+    public void deschideUsaSpate(){
+        System.out.println("Usa spate deschisa!");
+    }
+    public void liberUsaFata(){
+        System.out.println("Usa fata liber!");
+    }
+    public void liberUsaMijloc(){
+        System.out.println("Usa mijloc liber!");
+    }
+    public void liberUsaSpate(){
+        System.out.println("Usa spate liber!");
+    }
+}
